2edbe49273475fb55a6cc6cbaff6f197e3f44f64
Jminnovation <116853352+Jminnovation@users.noreply.github.com>
2023-05-29 22:00:36 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log("loading  javascript ......");
+
+function actionButton(){
+
+
+var myinput =document.getElementById("for","fi");
+
+    var myinput  = document.getElementById("B");
+    console.log(myinput);
+    
+    myinput.value = "En otro momento";
+    var myinput =document.getElementById("for");
+if(myinput.style.display == "none"){
+    myinput.style.display="block";
+}else{
+    myinput.style.display="none";
+ 
+ var myinput =document.getElementById("fi");
+
+if(myinput.style.display == "none"){
+    myinput.style.display="block";
+  
+}else{
+    myinput.style.display="none";
+}
+}
+}
+
+
+
